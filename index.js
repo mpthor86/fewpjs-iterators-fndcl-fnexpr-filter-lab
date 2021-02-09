681e2bcdc8ac21f9f71dfdc6db0401a9ecd698fb
michael <mpthornton86@gmail.com>
2021-02-09 15:19:04 -0700

    commit tree 1a1fd72566dbe9545754334651e875d52fce6232

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(arr, str) {
+    let driver = arr.filter(d => d.toLowerCase() === str.toLowerCase())
+    return driver
+}
+
+function fuzzyMatch(arr, str) {
+    let driver = arr.filter(d => d.charAt(0) === str.charAt(0))
+    return driver
+}
+
+function matchName(arr, str) {
+    let driver = arr.filter(d => d.name === str)
+    return driver
+}
